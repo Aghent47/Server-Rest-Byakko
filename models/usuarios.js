e8c3fe67b8740fa3e9180e61eba7020763b12a6a
Aghent47 <neiderkk@gmail.com>
2024-03-10 23:00:38 -0500

    mejorando esquema usuario

--- models/usuarios.js
+++ models/usuarios.js
@@ -83,5 +83,11 @@ const UsuarioSchema = new Schema({
 
 });
 
+UsuarioSchema.methods.toJSON = function() {
+    const {__v, password, _id, ...usuario} = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+}
+
 
 export const Usuario = model('Usuario', UsuarioSchema)
